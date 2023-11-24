5646f4d53f22f9e3847ff9d002d53c662886003d
Sathish013 <151933573+Sathish013@users.noreply.github.com>
2023-11-24 19:04:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My new healthy dinner')
+streamlit.title('My New Healthy Dinner')
+streamlit.header('Break Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
