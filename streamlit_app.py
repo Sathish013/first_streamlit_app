da7525a380052a53b7b589699862df2236e8780b
Sathish013 <151933573+Sathish013@users.noreply.github.com>
2023-11-24 19:06:34 +0530

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My New Healthy Dinner')
-streamlit.header('Break Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' 🥣 Break Menu')
+streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥑🍞 Hard-Boiled Free-Range Egg')
